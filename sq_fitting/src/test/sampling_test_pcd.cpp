c5b6d7bca27c1c8f226f7588b2a73d105b55e425
ommmid <heidomid@isu.edu>
2018-11-15 20:22:16 -0700

    Added "pcd_viewer.cpp". This node uses pcl/io to load a *.pcd file and then visulaize it with pcl/visualization. rosrun the node and enter the name of the pcd file you want to visualize.

--- sq_fitting/src/test/sampling_test_pcd.cpp
+++ sq_fitting/src/test/sampling_test_pcd.cpp
@@ -9,7 +9,15 @@
 int main(int argc, char *argv[])
 {
 
-  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_combined(new pcl::PointCloud<pcl::PointXYZRGB>());
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_combined(new pcl::PointCloud<pcl::PointXYZRGB>()); // ::Ptr or cloud_combined is a shared_ptr
+/*
+ => Use raw pointers when you do not want to have any ownership attached to the pointer. This job can also often be done with references. Raw pointers can also be used in
+some low level code (such as for implementing smart pointers, or implementing containers).
+=> Use unique_ptr or scope_ptr when you want unique ownership of the object. This is the most useful option, and should be used in most cases. Unique ownership
+can also be expressed by simply creating an object directly, rather than using a pointer (this is even better than using a unique_ptr, if it can be done).
+=> Use shared_ptr or intrusive_ptr when you want shared ownership of the pointer. This can be confusing and inefficient, and is often not a good option.
+Shared ownership can be useful in some complex designs, but should be avoided in general, because it leads to code which is hard to understand
+ */
 
   sq_fitting::sq super;
   super.a1 = 0.012925;
@@ -17,7 +25,7 @@ int main(int argc, char *argv[])
   super.a3 = 0.116491;
   super.e1 = 1.307882;
   super.e2 = 1.268144;
-  geometry_msgs::Pose pose;
+  geometry_msgs::Pose pose; // from sq.h: pose is a template so however we define the pose here (geometry_msgs::Pose) it is going to replace the template
   pose.position.x = 1.0;
   pose.position.y = 0.0;
   pose.position.z = 1.0;
@@ -39,22 +47,21 @@ int main(int argc, char *argv[])
   super2.pose = pose2;
 
   SuperquadricSampling *sam = new SuperquadricSampling(super);
-  sam->sample_pilu_fisher();
+  sam->sample_pilu_fisher(); // Pilu-Fisher is a method to do the sampling
   pcl::PointCloud<PointT>::Ptr cloud(new pcl::PointCloud<PointT>);
-  sam->getCloud(cloud);
+  sam->getCloud(cloud); // getCloud assigns the local PointCloud made by pilu_fisher method inside sam pointer object to the input PointCloud which is cloud2 here
   std::cout<<"size of the first sampled cloud: "<<cloud->points.size()<<std::endl;
 
-
-
   SuperquadricSampling *sam2 = new SuperquadricSampling(super2);
-  pcl::PointCloud<PointT>::Ptr cloud2(new pcl::PointCloud<PointT>);
   sam2->sample_pilu_fisher();
+  pcl::PointCloud<PointT>::Ptr cloud2(new pcl::PointCloud<PointT>);
   sam2->getCloud(cloud2);
   std::cout<<"size of the second sampled cloud: "<<cloud2->points.size()<<std::endl;
 
 
-  *cloud_combined = *cloud2;
+  *cloud_combined = *cloud2; // both are shared_ptr. This line means the value that cloud2 is pointing to is going to be assigned to the value that cloud_combined is pointing to
   //*cloud_combined+= *cloud2;
+
 
   std::cout<<"Size of combined superquadrics cloud: "<<cloud_combined->points.size()<<std::endl;
   pcl::io::savePCDFileASCII ("superquadrics.pcd", *cloud_combined);
